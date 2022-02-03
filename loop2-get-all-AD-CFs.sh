80aa25f53670495354242ae88ca60e058b1fe0b3
awkto <awkto@outlook.com>
2022-02-03 12:33:50 -0500

    added .exe when calling dnscmd. this is because WSL requires it. Cygwin on the other hand does not

--- loop2-get-all-AD-CFs.sh
+++ loop2-get-all-AD-CFs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 for domain in $(cat ad-domains.var); do \
-	dnscmd $(cat $domain-dcs.mid|shuf|head -1) /EnumZones \
+	dnscmd.exe $(cat $domain-dcs.mid|shuf|head -1) /EnumZones \
 		| grep Forwarder \
 		|awk {'print $1'} \
 		| tee $domain-fwdzones.mid ; \
 done
-
--- loop3-get-all-CFs-targets.sh
+++ loop3-get-all-CFs-targets.sh
@@ -7,7 +7,7 @@ for domain in $(cat ad-domains.var); do \
 	for zone in $(cat $domain-fwdzones.mid); do \
 		echo "ZONE "$zone \
 	                |tee -a $domain-cf-targets.out; \
-	        dnscmd $(cat $domain-dcs.mid|shuf|head -1) /ZoneInfo $zone  \
+	        dnscmd.exe $(cat $domain-dcs.mid|shuf|head -1) /ZoneInfo $zone  \
 	                |grep "Master\[" \
 	                |awk {'print $8'} \
 	                |tr -d 'addr=' \
